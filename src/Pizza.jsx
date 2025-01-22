0a874f8a4113bbcb5a671ac693ca173ac25f2591
carlos <c.delriofrances@gmail.com>
2025-01-22 16:55:15 -0300

    added tests!

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,9 +1,10 @@
 const Pizza = (props) => {
+  const fallbackImage = "https://picsum.photos/200";
   return (
     <div className="pizza">
       <h1>{props.name}</h1>
       <p>{props.description}</p>
-      <img src={props.image} alt={props.name} />
+      <img src={props.image || fallbackImage} alt={props.name} />
     </div>
   );
 };
